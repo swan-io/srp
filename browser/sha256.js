739d18757bf6c204b5f152abd2804ee589159dbf
Mathieu Acthernoene <zoontek@gmail.com>
2021-10-17 13:19:29 +0200

    Remove imports

--- browser/sha256.js
+++ browser/sha256.js
@@ -1,12 +1,32 @@
 "use strict";
 
-const arrayBufferToHex = require("array-buffer-to-hex");
-const hexToArrayBuffer = require("hex-to-array-buffer");
 const rawSha256 = require("crypto-digest-sync/sha256");
+const SRPInteger = require("../lib/srp-integer");
+
+function bufferToHex(buffer) {
+  let hex = "";
+  const array = new Uint8Array(buffer);
+
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i].toString(16);
+    hex += value.length === 1 ? "0" + value : value;
+  }
+
+  return hex;
+}
+
+function hexToBuffer(hex) {
+  hex = hex.length % 2 !== 0 ? "0" + hex : hex;
+  const array = new Uint8Array(hex.length / 2);
+
+  for (let i = 0; i < hex.length; i += 2) {
+    array[i / 2] = parseInt(hex.substring(i, i + 2), 16);
+  }
+
+  return array.buffer;
+}
 
 const encodeUtf8 = TextEncoder.prototype.encode.bind(new TextEncoder());
-
-const SRPInteger = require("../lib/srp-integer");
 
 function concat(buffers) {
   const length = buffers.reduce((mem, item) => mem + item.byteLength, 0);
@@ -27,7 +47,7 @@ module.exports = function sha256(...args) {
   const buffer = concat(
     args.map((arg) => {
       if (arg instanceof SRPInteger) {
-        return hexToArrayBuffer(arg.toHex());
+        return hexToBuffer(arg.toHex());
       } else if (typeof arg === "string") {
         return encodeUtf8(arg);
       } else {
@@ -36,5 +56,5 @@ module.exports = function sha256(...args) {
     }),
   );
 
-  return SRPInteger.fromHex(arrayBufferToHex(rawSha256(buffer)));
+  return SRPInteger.fromHex(bufferToHex(rawSha256(buffer)));
 };
